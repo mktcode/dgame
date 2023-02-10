609514238781ad25d16d5561373628e00a06e8ee
mktcode <kontakt@markus-kottlaender.de>
2023-02-10 23:06:56 +0100

    changes

--- src/components/BuildMenu.vue
+++ src/components/BuildMenu.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="grow overflow-y-auto bg-sky-900">
-    <div class="grid grid-cols-2 text-center text-white">
-      <div class="aspect-square bg-sky-900 flex items-center justify-center">
-        <div class="animate-pulse text-2xl font-bold">
+    <div class="grid grid-cols-2 text-center">
+      <div
+        class="aspect-square bg-sky-900 flex items-center justify-center bg-cover cursor-pointer"
+        :style="{
+          backgroundImage: 'url(artwork/base2.jpeg)',
+        }"
+      >
+        <div class="animate-pulse text-2xl font-bold bg-gray-50 bg-opacity-30 text-white grow">
           Deploy Base
         </div>
       </div>
-      <div class="aspect-square bg-sky-700"></div>
-      <div class="aspect-square bg-sky-800"></div>
+      <div class="aspect-square bg-sky-700 flex items-center justify-center font-bold text-sky-600 text-3xl">
+        ?
+      </div>
     </div>
   </div>
 </template>
--- src/state/GameMap.ts
+++ src/state/GameMap.ts
@@ -44,7 +44,7 @@ const tilesInfo = ref<{[z: number]: {[y: number]: {[x: number]: TileInfo}}}>({
 
 const gameMap = ref<HTMLElement | null>(null);
 const position = ref({ x: 0, y: 0, z: 0 });
-const selectedTile = ref<{ x: number, y: number, z: number } | null>({ x: 0, y: 0, z: 0 });
+const selectedTile = ref<{ x: number, y: number, z: number } | null>(null);
 
 const selectedTileInfo = computed<TileInfo | null>(() => {
   if (selectedTile.value === null) {
