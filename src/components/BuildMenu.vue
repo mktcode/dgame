543918559f34b53b6b18f9e738ce49d081c09bb8
mktcode <kontakt@markus-kottlaender.de>
2023-02-10 06:34:53 +0100

    deploy base button

--- src/components/BuildMenu.vue
+++ src/components/BuildMenu.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="grow overflow-y-auto bg-gray-400">
     <div class="grid grid-cols-2 text-center text-white">
-      <div class="aspect-square bg-sky-500"></div>
+      <div class="aspect-square bg-sky-500 flex items-center justify-center">
+        <div class="animate-pulse text-2xl font-bold">
+          Deploy Base
+        </div>
+      </div>
       <div class="aspect-square bg-sky-200"></div>
       <div class="aspect-square bg-sky-200"></div>
       <div class="aspect-square bg-sky-200"></div>
