c75879f089aa353ea31915150f3148d8eeedec33
mktcode <kontakt@markus-kottlaender.de>
2023-02-13 15:40:45 +0100

    configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,11 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  env: {
+    es2020: true,
+    node: true,
+    browser: true,
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
