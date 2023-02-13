f2f04b38ef176a11fe5bcc68b4ed15892db3c060
mktcode <kontakt@markus-kottlaender.de>
2023-02-13 15:43:56 +0100

    linting

--- env.d.ts
+++ env.d.ts
@@ -4,4 +4,4 @@ declare interface Window {
   ethereum: any;
 }
 
-declare module '@jamescoyle/vue-icon';
+declare module "@jamescoyle/vue-icon";
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -45,8 +45,15 @@ onUnmounted(() => {
 
 <template>
   <div
-    class="relative flex aspect-square w-40 min-w-[6rem] cursor-pointer border-sky-900 items-center justify-center overflow-hidden rounded-lg bg-sky-900 text-sm transition-all"
-    :class="{ 'bg-opacity-10 hover:bg-opacity-20': !tileInfo, 'border-2': selectedTile && selectedTile.x === x && selectedTile.y === y && selectedTile.z === z }"
+    class="relative flex aspect-square w-40 min-w-[6rem] cursor-pointer items-center justify-center overflow-hidden rounded-lg border-sky-900 bg-sky-900 text-sm transition-all"
+    :class="{
+      'bg-opacity-10 hover:bg-opacity-20': !tileInfo,
+      'border-2':
+        selectedTile &&
+        selectedTile.x === x &&
+        selectedTile.y === y &&
+        selectedTile.z === z,
+    }"
     @click="selectedTile = { x, y, z }"
   >
     <Transition>
