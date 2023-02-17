0b938095971edc0f1e8de58277e3633fef6db7f1
mktcode <kontakt@markus-kottlaender.de>
2023-02-17 17:05:29 +0100

    minimum responsiveness

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,27 @@ import TileInfo from "./components/TileInfo.vue";
 import { GameMapState } from "./state/GameMap";
 import { IS_ETHEREUM_ENABLED, useWeb3Account } from "./state/useWeb3Account";
 
-const { position } = GameMapState();
-const { accountAddress, connect, shortenAddress } = useWeb3Account();
+const { position, selectedTile } = GameMapState();
+const { accountAddress, connect, shortenAddress } = useWeb3Account();;
 </script>
 
 <template>
   <div class="flex h-screen">
     <GameMap />
-    <div class="flex w-72 flex-none flex-col">
-      <div v-if="accountAddress" class="py-1 text-center text-sky-700">
+    <div
+      class="flex overflow-hidden flex-none flex-col w-full absolute inset-0 sm:relative sm:w-72"
+      :class="{
+        'hidden sm:flex': !selectedTile,
+      }"
+    >
+      <ResourcesInfo v-if="accountAddress" />
+      <div v-if="accountAddress" class="py-1 text-center text-sky-600 bg-sky-900">
         {{ shortenAddress(accountAddress) }}
       </div>
       <button v-else-if="IS_ETHEREUM_ENABLED" @click="connect">
         Connect Wallet
       </button>
-      <ResourcesInfo v-if="accountAddress" />
-      <div class="py-1 text-center text-sky-700">
+      <div class="py-1 text-center text-sky-600 bg-sky-900">
         x: {{ position.x.toString() }} y: {{ position.y.toString() }} z:
         {{ position.z.toString() }}
       </div>
