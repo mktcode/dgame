0067315a2c32b8a453b76a4255afee66cdf4ddd9
mktcode <kontakt@markus-kottlaender.de>
2023-02-17 14:48:52 +0100

    updated to new web3gun implementation

--- src/state/Indexer.ts
+++ src/state/Indexer.ts
@@ -1,50 +1,54 @@
-import { Web3GunClient } from 'web3gun';
-import DGAME_ABI from '../contracts/DGame.json';
+import { Web3GunClient } from "@web3gun/client";
+import DGAME_ABI from "../contracts/DGame.json";
 
 const ADDRESS = import.meta.env.VITE_DGAME_CONTRACT_ADDRESS;
-const PROVIDER_URL = "https://goerli.infura.io/v3/aa26f7213d2046c3a20c750679140729";
+const PROVIDER_URL =
+  "https://goerli.infura.io/v3/aa26f7213d2046c3a20c750679140729";
 
-export const indexer = new Web3GunClient(PROVIDER_URL);
+export const indexer = new Web3GunClient(PROVIDER_URL, [
+  "https://gun.mktcode.uber.space",
+]);
 
 indexer.contract(ADDRESS, DGAME_ABI, async (dgameContract, storage) => {
-  dgameContract.on("TokenMinted", async (tokenId: bigint, owner: string, x: bigint, y: bigint, z: bigint) => {
-    owner = owner.toLowerCase();
+  dgameContract.on(
+    "TokenMinted",
+    async (tokenId: bigint, owner: string, x: bigint, y: bigint, z: bigint) => {
+      owner = owner.toLowerCase();
 
-    const ownerBalance = await dgameContract.balanceOf(owner);
-  
-    storage.get("balances").get(owner).put(ownerBalance.toString());
-    storage
-      .get("tokens")
-      .get(tokenId.toString())
-      .get("owner")
-      .put(owner);
+      const ownerBalance = await dgameContract.balanceOf(owner);
 
-    storage.get("tokens").get(tokenId.toString()).get("level").put("0");
-    storage.get("tokens").get(tokenId.toString()).get("type").put("base");
-    storage.get("tokens").get(tokenId.toString()).get("name").put("Base");
+      storage.get("balances").get(owner).put(ownerBalance.toString());
+      storage.get("tokens").get(tokenId.toString()).get("owner").put(owner);
+      storage.get("tokens").get(tokenId.toString()).get("level").put("0");
+      console.log(`Reset level of token ${tokenId} to 0`);
+      storage.get("tokens").get(tokenId.toString()).get("type").put("base");
+      storage.get("tokens").get(tokenId.toString()).get("name").put("Base");
+      storage
+        .get("tokens")
+        .get(tokenId.toString())
+        .get("description")
+        .put("A player's base");
+      storage
+        .get("tokens")
+        .get(tokenId.toString())
+        .get("image")
+        .put("artwork/base2.jpeg");
 
-    storage
-      .get("tokens")
-      .get(tokenId.toString())
-      .get("description")
-      .put("A player's base");
-
-    storage
-      .get("tokens")
-      .get(tokenId.toString())
-      .get("image")
-      .put("artwork/base2.jpeg");
-
-    storage
-      .get("coords")
-      .get(x.toString())
-      .get(y.toString())
-      .get(z.toString())
-      .put(tokenId.toString());
-  });
+      storage
+        .get("coords")
+        .get(x.toString())
+        .get(y.toString())
+        .get(z.toString())
+        .put(tokenId.toString());
+    }
+  );
 
   dgameContract.on("LevelUp", async (tokenId: bigint, newLevel: bigint) => {
-    storage.get("tokens").get(tokenId.toString()).get("level").put(newLevel.toString());
+    storage
+      .get("tokens")
+      .get(tokenId.toString())
+      .get("level")
+      .put(newLevel.toString());
   });
 });
 
